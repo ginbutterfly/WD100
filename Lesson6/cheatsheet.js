71ea1eeab5ff1251df2ac3e7b3534947157191bb
Ginger Blumenthal <ginbutterfly@gmail.com>
2016-09-20 17:30:27 -0400

    add Lesson 6

---
+++ Lesson6/cheatsheet.js
@@ -0,0 +1,23 @@
+// Create a "cheat sheet" JavaScript file that contains working examples with comments of:
+
+// - Variables
+
+var name = "Emma";
+var breed = "Cavalier King Charles Spaniel";
+
+
+// - Debugging (alerts, console.log, logging to HTML)
+
+alert(name + " the " + breed);
+
+
+
+// - Different data types
+
+// - Arrays
+
+// - Testing
+
+// - Logic
+
+// - Functions
